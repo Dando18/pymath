27a70c7cd8ee4553c671133384d91b228abb982f
Daniel Nichols <dando18studios@gmail.com>
2016-12-18 14:38:22 -0500

    updated Polynomial test

--- Test.py
+++ Test.py
@@ -8,10 +8,15 @@ mon0 = Monomial(1, 2)
 mon1 = Monomial(1, 1)
 mon2 = Monomial(-1, 0)
 
-pol0 = Polynomial([mon0, mon1, mon2])
+pol0 = Polynomial([mon0, mon1])
+pol0.add(mon2)
+pol1 = Polynomial([Monomial(1, 2), Monomial(1, 0)])
 
-#print(pol0.pretty_print())
-#print(pol0.print_eval(5))
+
+print(pol0.pretty_print())
+print(pol0.print_eval(5))
+print(pol0 - pol1)
+print(pol1 * pol0)
 
 
 #linear interpolation
